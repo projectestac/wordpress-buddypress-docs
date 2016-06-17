5eb06ce79d816d801aca95cf981e06cdf4c58fb4
Toni Ginard <toni.ginard@gmail.com>
2016-06-17 12:52:52 +0200

    Recovered missing XTEC change

--- bp-docs.php
+++ bp-docs.php
@@ -315,7 +315,16 @@ class BP_Docs {
 			'rewrite'      => array(
 				'slug'       => bp_docs_get_docs_slug(),
 				'with_front' => false
-   			)
+
+// XTEC ************ MODIFICAT - Don't show option in admin menu because of custom menu
+// 2014.07.21 @aginard
+			),
+			'show_in_menu'        => false,
+//************ ORIGINAL
+/*
+			)
+*/
+//************ FI
 
         ) );
 
